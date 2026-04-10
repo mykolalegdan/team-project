b897d761ab3bc09fca89ac500ef74f56129965fa
yuriivkrylchuk <yurii.v.krylchuk@ukd.edu.ua>
2026-04-10 16:00:24 +0300

    changed the logic of the entire program

--- lol.py
+++ lol.py
@@ -1,15 +1,21 @@
-contacts = []  # Наш список для зберігання
+contacts = []
 
 while True:
-    action = input("\n1 - Додати, 2 - Список, 0 - Вихід: ")
-
+    action = input("\n1-Додати, 2-Список, 3-Видалити, 0-Вихід: ")
     if action == "1":
-        name = input("Ім'я: ")
-        contacts.append(name)
-        print("Збережено!")
+        name = input("ПІБ: ")
+        phone = input("Номер: ")
+        contacts.append({"name": name, "phone": phone})
+        print("Додано!")
 
     elif action == "2":
-        print("Ваші контакти:", contacts)
+        for c in contacts:
+            print(f"{c['name']}: {c['phone']}")
+
+    elif action == "3":
+        name = input("Кого видалити? (ПІБ): ")
+        contacts = [c for c in contacts if c['name'] != name]
+        print("Оновлено.")
 
     elif action == "0":
         break
